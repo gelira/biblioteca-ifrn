b1ba6682221d8b82fb3b4c0117843ffa7f9913e3
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-07-22 20:28:40 -0300

    Alterando modelo usuario

--- services/autenticacao/autenticacaoapp/models/usuario.py
+++ services/autenticacao/autenticacaoapp/models/usuario.py
@@ -9,9 +9,9 @@ class Usuario(models.Model):
         default=uuid.uuid4)
     user = models.OneToOneField(
         to=settings.AUTH_USER_MODEL, 
-        on_delete=models.CASCADE)
-    matricula = models.CharField(max_length=20)
+        on_delete=models.PROTECT)
     nome = models.CharField(max_length=150)
+    nome_completo = models.CharField(max_length=150)
     email_institucional = models.EmailField()
     email_pessoal = models.EmailField(blank=True)
     vinculo = models.CharField(max_length=20)
