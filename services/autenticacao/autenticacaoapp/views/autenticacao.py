646e1c0baa474eebac9a1b77a66cbdfdfb801006
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-07-22 21:40:48 -0300

    Serializador SUAP na view

--- services/autenticacao/autenticacaoapp/views/autenticacao.py
+++ services/autenticacao/autenticacaoapp/views/autenticacao.py
@@ -1,9 +1,10 @@
 from rest_framework_simplejwt.views import TokenViewBase
 
-from ..serializers import ObterTokenSerializer, VerificarTokenSerializer
+from ..serializers import (
+    ObterTokenSerializer, ObterTokenSUAPSerializer, VerificarTokenSerializer)
 
 class ObterTokenView(TokenViewBase):
-    serializer_class = ObterTokenSerializer
+    serializer_class = ObterTokenSUAPSerializer
 
 class VerificarTokenView(TokenViewBase):
     serializer_class = VerificarTokenSerializer
