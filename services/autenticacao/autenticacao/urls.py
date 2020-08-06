029dbf1bbd5e0bc69effc72b6e654ebc454002fe
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-08-05 22:39:27 -0300

    Rotas básicas dos perfis

--- services/autenticacao/autenticacao/urls.py
+++ services/autenticacao/autenticacao/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
 from autenticacaoapp import views
+
+router = SimpleRouter(trailing_slash=False)
+router.register('perfis', views.PerfilViewSet)
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('token', views.ObterTokenView.as_view()),
     path('verificar', views.VerificarTokenView.as_view()),
     path('informacoes', views.InformacoesUsuarioView.as_view())
-]
+] + router.urls
--- services/autenticacao/autenticacaoapp/serializers/__init__.py
+++ services/autenticacao/autenticacaoapp/serializers/__init__.py
@@ -2,3 +2,4 @@ from .token import (
     ObterTokenSerializer, ObterTokenSUAPSerializer, VerificarTokenSerializer
 )
 from .usuario import UsuarioSerializer
+from .perfil import PerfilSerializer
