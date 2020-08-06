4d173b7e2fd6179a769b47f7afd01293159840f4
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-08-05 23:33:59 -0300

    Garantindo um perfil padrão único

--- services/autenticacao/autenticacaoapp/serializers/perfil.py
+++ services/autenticacao/autenticacaoapp/serializers/perfil.py
@@ -3,6 +3,21 @@ from rest_framework import serializers
 from ..models import Perfil
 
 class PerfilSerializer(serializers.ModelSerializer):
+    def assegurarPerfilPadraoUnico(self, data):
+        if Perfil.objects.filter(padrao=True).count() == 0:
+            data['padrao'] = True
+        elif data['padrao']:
+            Perfil.objects.filter(padrao=True).update(padrao=False)
+        return data
+
+    def create(self, data):
+        data = self.assegurarPerfilPadraoUnico(data)
+        return super().create(data)
+
+    def update(self, instance, data):
+        data = self.assegurarPerfilPadraoUnico(data)
+        return super().update(instance, data) 
+
     class Meta:
         model = Perfil
         fields = '__all__'
