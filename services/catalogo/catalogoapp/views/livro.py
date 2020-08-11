40c8a4e4ff01bbe7df1e002868b6df337ac919bc
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-08-11 12:55:11 -0300

    Buscar livro pelo uuid

--- services/catalogo/catalogoapp/views/livro.py
+++ services/catalogo/catalogoapp/views/livro.py
@@ -1,4 +1,6 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.decorators import action
 
 from ..models import Livro
 from ..serializers import LivroSerializer
@@ -7,3 +9,7 @@ class LivroViewSet(viewsets.ModelViewSet):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
     permission_classes = []
+    lookup_value_regex = '[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{12}'
+    
+    def get_object(self):
+        return get_object_or_404(self.queryset, _id=self.kwargs['pk'])
