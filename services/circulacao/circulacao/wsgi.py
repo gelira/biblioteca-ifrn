883adcdae207757dd6ecbc74e1a7a5cbf17be4d1
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-08-30 20:38:34 -0300

    serviço circulacao

---
+++ services/circulacao/circulacao/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for circulacao project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'circulacao.settings')
+
+application = get_wsgi_application()
