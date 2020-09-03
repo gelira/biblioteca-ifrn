a653c4bcd9df5cbf0d82653a618a3f52afd71fa5
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-09-02 22:49:41 -0300

    Carregando lista de permissões no serializador

--- services/autenticacao/autenticacaoapp/serializers/usuario.py
+++ services/autenticacao/autenticacaoapp/serializers/usuario.py
@@ -1,15 +1,23 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import SerializerMethodField
 
-from ..models import Usuario
+from ..models import Usuario, Permissao
 from .perfil import PerfilSerializer
 
 class UsuarioSerializer(ModelSerializer):
     matricula = SerializerMethodField()
     perfil = PerfilSerializer()
+    lista_permissoes = SerializerMethodField()
 
     def get_matricula(self, obj):
         return obj.user.username
+
+    def get_lista_permissoes(self, obj):
+        retorno = []
+        qs = Permissao.objects.filter(usuarios__usuario_id=obj.id)
+        for perm in qs.all():
+            retorno.append(perm.codigo)
+        return retorno
 
     class Meta:
         model = Usuario
