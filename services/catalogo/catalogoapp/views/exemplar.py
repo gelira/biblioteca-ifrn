e02a7132a6d391a60a72bd79c3338e06c99186df
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-09-06 12:55:21 -0300

    Aplicando permissões viewsets catalogo

--- services/catalogo/catalogoapp/views/exemplar.py
+++ services/catalogo/catalogoapp/views/exemplar.py
@@ -2,8 +2,12 @@ from rest_framework import viewsets
 
 from ..models import Exemplar
 from ..serializers import ExemplarSerializer
+from ..permissions import (
+    AutenticadoPermissao,
+    LivroModificarPermissao
+)
 
 class ExemplarViewSet(viewsets.ModelViewSet):
     queryset = Exemplar.objects.all()
     serializer_class = ExemplarSerializer
-    permission_classes = []
+    permission_classes = [AutenticadoPermissao, LivroModificarPermissao]
