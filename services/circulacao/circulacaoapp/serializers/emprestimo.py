4bd74c3d35a8d998e51ffb3b2e2dfb862430c00f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-09-16 13:05:58 -0300

    Revert "Removendo map desnecessário"
    This reverts commit 3ba6d791ee5ae567571a3a09c3560823fca0fb85.

--- services/circulacao/circulacaoapp/serializers/emprestimo.py
+++ services/circulacao/circulacaoapp/serializers/emprestimo.py
@@ -160,7 +160,7 @@ class EmprestimoCreateSerializer(serializers.Serializer):
         if (len(emprestimos_vigentes) + quantidade_livros) > max_livros:
             raise serializers.ValidationError('Atingido o limite de livros para o usuário')
 
-        return emprestimos_vigentes
+        return list(map(lambda x: str(x), emprestimos_vigentes))
 
     def emprestar_exemplar_referencia(self):
         hoje = date.today()
