c97ab77d95596f226f50699f6b623af89e2c60ee
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-01 11:11:36 -0300

    Action devolução dos livros

--- services/circulacao/circulacaoapp/views/emprestimo.py
+++ services/circulacao/circulacaoapp/views/emprestimo.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from ..models import Emprestimo
-from ..serializers import EmprestimoCreateSerializer
+from ..serializers import (
+    EmprestimoCreateSerializer,
+    DevolucaoEmprestimosSerializer
+)
 from ..permissions import (
     AutenticadoPermissao,
     FazerEmprestimoPermissao
@@ -10,13 +14,24 @@ from ..permissions import (
 
 class EmprestimoViewSet(ModelViewSet):
     queryset = Emprestimo.objects.all()
-    serializer_class = EmprestimoCreateSerializer
     permission_classes = [
         AutenticadoPermissao,
         FazerEmprestimoPermissao
     ]
 
+    def get_serializer_class(self):
+        if self.action == 'devolucoes':
+            return DevolucaoEmprestimosSerializer
+        return EmprestimoCreateSerializer
+
     def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(status=200)
+
+    @action(methods=['post'], detail=False, url_path='devolucoes')
+    def devolucoes(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
