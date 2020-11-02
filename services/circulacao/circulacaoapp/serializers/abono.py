c1dc7dc422e2a9d27c29dba3a2498a5895632243
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-02 20:19:56 -0300

    Salvando abono nas suspensões

--- services/circulacao/circulacaoapp/serializers/abono.py
+++ services/circulacao/circulacaoapp/serializers/abono.py
@@ -1,21 +1,62 @@
+from django.db import transaction
 from rest_framework import serializers
 
-from ..models import Abono
+from ..models import (
+    Abono,
+    Suspensao
+)
 
 class AbonoCreateSerializer(serializers.ModelSerializer):
+    suspensoes = serializers.ListField(
+        child=serializers.UUIDField(),
+        allow_empty=False,
+        write_only=True
+    )
+
     def validate(self, data):
-        data['usuario_id'] = self.context['request'].user['_id']
-        print(data)
+        suspensoes_id = data['suspensoes']
+        suspensoes = []
+        usuarios = {}
+
+        for s_id in suspensoes_id:
+            suspensao = Suspensao.objects.filter(**{
+                '_id': s_id,
+                'abono_id': None
+            }).first()
+            
+            if suspensao is not None:
+                usuario_id = str(suspensao.usuario_id)
+                if usuario_id not in usuarios:
+                    usuarios[usuario_id] = 0
+                
+                usuarios[usuario_id] += suspensao.total_dias
+                suspensoes.append(s_id)
+
+        if not usuarios:
+            raise serializers.ValidationError('Nenhuma suspensão encontrada')
+
+        data.update({
+            'usuario_id': self.context['request'].user['_id'],
+            'usuarios': usuarios,
+            'suspensoes': suspensoes
+        })
         return data
 
     def create(self, data):
-        return Abono.objects.create(
-            usuario_id=data['usuario_id'],
-            justificativa=data['justificativa']
-        )
+        abono = None
+        with transaction.atomic():
+            abono = Abono.objects.create(
+                usuario_id=data['usuario_id'],
+                justificativa=data['justificativa']
+            )
+            Suspensao.objects.filter(_id__in=data['suspensoes']).update(abono_id=abono.pk)
+
+        print(data['usuarios'])
+        return abono
     
     class Meta:
         model = Abono
         fields = [
+            'suspensoes',
             'justificativa'
         ]
