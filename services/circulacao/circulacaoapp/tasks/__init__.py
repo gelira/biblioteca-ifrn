40847b7853c2d4529cc9079be315da890cf13966
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-02 14:08:13 -0300

    Retirando try excepts desnecessários

--- services/circulacao/circulacaoapp/tasks/__init__.py
+++ services/circulacao/circulacaoapp/tasks/__init__.py
@@ -9,21 +9,12 @@ from circulacaoapp.tasks.usuario import _usuarios_suspensos
 
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def marcar_exemplares_emprestados(usuario_id, codigos):
-    try:
-        _marcar_exemplares_emprestados(usuario_id, codigos)
-    except Exception as e:
-        raise self.retry(exc=e, countdown=10)
+    _marcar_exemplares_emprestados(usuario_id, codigos)
 
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def marcar_exemplares_devolvidos(usuario_id, codigos):
-    try:
-        _marcar_exemplares_devolvidos(usuario_id, codigos)
-    except Exception as e:
-        raise self.retry(exc=e, countdown=10)
+    _marcar_exemplares_devolvidos(usuario_id, codigos)
 
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def usuarios_suspensos(usuario_id, usuarios):
-    try:
-        _usuarios_suspensos(usuario_id, usuarios)
-    except Exception as e:
-        raise self.retry(exc=e, countdown=10)
+    _usuarios_suspensos(usuario_id, usuarios)
