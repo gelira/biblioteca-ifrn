c225b806acc16e4cd58447225a5184ea7e8c4b5a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-04 07:07:52 -0300

    Contagem de exemplares disponíveis

--- services/catalogo/catalogoapp/serializers/livro.py
+++ services/catalogo/catalogoapp/serializers/livro.py
@@ -30,6 +30,7 @@ class LivroListSerializer(serializers.ModelSerializer):
 class LivroRetrieveSerializer(serializers.ModelSerializer):
     foto_capa = serializers.SerializerMethodField()
     exemplares = serializers.SerializerMethodField()
+    exemplares_disponiveis = serializers.SerializerMethodField()
     localizacoes_fisicas = serializers.SerializerMethodField()
     indexadores = serializers.SerializerMethodField()
 
@@ -43,6 +44,9 @@ class LivroRetrieveSerializer(serializers.ModelSerializer):
         exemplares = obj.exemplares.filter(ativo=True).all()
         serializer = ExemplarListSerializer(exemplares, many=True)
         return serializer.data
+
+    def get_exemplares_disponiveis(self, obj):
+        return obj.exemplares.filter(ativo=True, disponivel=True, referencia=False).count()
 
     def get_localizacoes_fisicas(self, obj):
         localizacoes = obj.localizacoes.filter(ativo=True).all()
