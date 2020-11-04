09bc73203635a6c430c143e98d76cf2b28f7a98b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-04 07:46:23 -0300

    Modularizando verificação

--- services/circulacao/circulacaoapp/serializers/reserva.py
+++ services/circulacao/circulacaoapp/serializers/reserva.py
@@ -7,29 +7,31 @@ from ..models import Reserva
 CATALOGO_SERVICE_URL = os.getenv('CATALOGO_SERVICE_URL')
 
 class ReservaCreateSerializer(serializers.ModelSerializer):
-    def create(self, data):
-        return Reserva.objects.create(
-            livro_id=data['livro_id'],
-            usuario_id=self.context['request'].user['_id']
-        )
-    
     def validate_livro_id(self, value):
+        self.validar_livro(str(value))
+        return value
+
+    def validar_livro(self, livro_id):
         try:
-            r = requests.get(CATALOGO_SERVICE_URL + '/livros/' + str(value))
+            r = requests.get(CATALOGO_SERVICE_URL + '/livros/' + livro_id)
             if not r.ok:
                 raise serializers.ValidationError('Livro não encontrado')
 
             livro = r.json()
             if livro['exemplares_disponiveis'] > 0:
                 raise serializers.ValidationError('Há exemplares desse livro disponíveis')
-
-            return value
         
         except serializers.ValidationError as e:
             raise e
 
         except:
             raise serializers.ValidationError('Serviço demorou muito a responder')
+
+    def create(self, data):
+        return Reserva.objects.create(
+            livro_id=data['livro_id'],
+            usuario_id=self.context['request'].user['_id']
+        )
 
     class Meta:
         model = Reserva
