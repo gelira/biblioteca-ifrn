d5cffef3a6f7334d05d71ec31e245a568cc6e043
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-04 07:41:39 -0300

    validando exemplares disponíveis

--- services/circulacao/circulacaoapp/serializers/reserva.py
+++ services/circulacao/circulacaoapp/serializers/reserva.py
@@ -1,6 +1,10 @@
+import os
+import requests
 from rest_framework import serializers
 
 from ..models import Reserva
+
+CATALOGO_SERVICE_URL = os.getenv('CATALOGO_SERVICE_URL')
 
 class ReservaCreateSerializer(serializers.ModelSerializer):
     def create(self, data):
@@ -9,6 +13,24 @@ class ReservaCreateSerializer(serializers.ModelSerializer):
             usuario_id=self.context['request'].user['_id']
         )
     
+    def validate_livro_id(self, value):
+        try:
+            r = requests.get(CATALOGO_SERVICE_URL + '/livros/' + str(value))
+            if not r.ok:
+                raise serializers.ValidationError('Livro não encontrado')
+
+            livro = r.json()
+            if livro['exemplares_disponiveis'] > 0:
+                raise serializers.ValidationError('Há exemplares desse livro disponíveis')
+
+            return value
+        
+        except serializers.ValidationError as e:
+            raise e
+
+        except:
+            raise serializers.ValidationError('Serviço demorou muito a responder')
+
     class Meta:
         model = Reserva
         fields = [
