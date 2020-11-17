06c34b211b5f5c9fc6292bd7551d46bf905ba018
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-17 07:41:24 -0300

    Autenticação com Redis

--- services/avaliacao/avaliacao/settings.py
+++ services/avaliacao/avaliacao/settings.py
@@ -132,4 +132,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'avaliacaoapp.authentication.RedisAutenticacao'
+    ],
 }
