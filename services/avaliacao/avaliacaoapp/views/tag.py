14640992d604cefce272da5e22a39e237c8d546e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-11-19 22:15:01 -0300

    Modificando import ModelViewSet

--- services/avaliacao/avaliacaoapp/views/tag.py
+++ services/avaliacao/avaliacaoapp/views/tag.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
 
 from ..models import Tag
 from ..permissions import AutenticadoPermissao
 from ..serializers import TagCreateSerializer
 
-class TagViewSet(viewsets.ModelViewSet):
+class TagViewSet(ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagCreateSerializer
     permission_classes = [
