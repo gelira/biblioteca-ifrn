6744de42b5c535df1a02f0307651410c0bc40ed3
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-12-05 10:35:04 -0300

    Model CodigoPromocao

---
+++ services/autenticacao/autenticacaoapp/models/codigo_promocao.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from .timestamped import TimestampedModel
+from .usuario import Usuario
+
+class CodigoPromocao(TimestampedModel):
+    codigo = models.CharField(
+        max_length=20,
+        unique=True,
+        null=True
+    )
+    codigo_utilizado = models.CharField(
+        max_length=20,
+        blank=True
+    )
+    validade = models.DateTimeField()
+    usuario = models.ForeignKey(
+        to=Usuario,
+        on_delete=models.PROTECT,
+        related_name='codigos_promocao'
+    )
+    bolsista = models.ForeignKey(
+        to=Usuario,
+        on_delete=models.PROTECT,
+        related_name='codigos_promocao_utilizados',
+        null=True
+    )
+
+    class Meta:
+        db_table = 'codigos_promocao'
