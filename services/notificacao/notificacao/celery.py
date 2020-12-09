679d0f209ac2da81bf7ba473c100c4a1cd54f92e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2020-12-09 13:40:07 -0300

    módulo celery

---
+++ services/notificacao/notificacao/celery.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+
+import os
+import dotenv
+from celery import Celery
+
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nofiticacao.settings')
+
+app = Celery('nofiticacao')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
