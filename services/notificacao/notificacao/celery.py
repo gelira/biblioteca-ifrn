544c49820b2ba1bdf1d157ee493314fe92b5ce32
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-01-13 18:06:57 -0300

    Correção do nome do módulo

--- services/notificacao/notificacao/celery.py
+++ services/notificacao/notificacao/celery.py
@@ -5,7 +5,7 @@ import dotenv
 from celery import Celery
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nofiticacao.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notificacao.settings')
 
 app = Celery('nofiticacao')
 
