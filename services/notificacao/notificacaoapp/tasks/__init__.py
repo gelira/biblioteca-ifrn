274c3214c19285cc4f455acc4ce2fa8bdbad912f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-03-07 11:03:30 -0300

    email comprovante de devolução

--- services/notificacao/notificacaoapp/tasks/__init__.py
+++ services/notificacao/notificacaoapp/tasks/__init__.py
@@ -2,9 +2,14 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 from notificacaoapp.tasks.mensagens import (
-    _comprovante_emprestimo
+    _comprovante_emprestimo,
+    _comprovante_devolucao
 )
 
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def comprovante_emprestimo(contexto, emails):
     _comprovante_emprestimo(contexto, emails)
+
+@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
+def comprovante_devolucao(contexto, emails):
+    _comprovante_devolucao(contexto, emails)
