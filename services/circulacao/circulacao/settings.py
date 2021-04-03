bea59c020451e83179438b397dfff34c59fc23a4
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-03 13:54:39 -0300

    Instalação django-celery-beat

--- services/circulacao/circulacao/settings.py
+++ services/circulacao/circulacao/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'circulacaoapp',
     'rest_framework',
+    'django_celery_beat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
