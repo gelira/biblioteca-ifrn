abfcd8764afe9673c0a8a1c27703d4b116a6c130
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-04 15:46:38 -0300

    Removendo tasks desnecessárias circulacao

--- services/circulacao/circulacaoapp/tasks/__init__.py
+++ services/circulacao/circulacaoapp/tasks/__init__.py
@@ -2,13 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from celery import shared_task
 from circulacaoapp.tasks.emprestimo import (
-    _marcar_exemplares_emprestados,
-    _marcar_exemplares_devolvidos,
     _emprestimo_avaliado
-)
-from circulacaoapp.tasks.usuario import (
-    _usuarios_suspensos,
-    _usuarios_abono
 )
 from circulacaoapp.tasks.reserva import (
     _verificar_reservas
@@ -16,22 +10,6 @@ from circulacaoapp.tasks.reserva import (
 from circulacaoapp.tasks.devolucao import (
     _enviar_comprovantes_devolucao
 )
-
-@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
-def marcar_exemplares_emprestados(codigos):
-    _marcar_exemplares_emprestados(codigos)
-
-@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
-def marcar_exemplares_devolvidos(codigos):
-    _marcar_exemplares_devolvidos(codigos)
-
-@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
-def usuarios_suspensos(usuarios):
-    _usuarios_suspensos(usuarios)
-
-@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
-def usuarios_abono(usuarios):
-    _usuarios_abono(usuarios)
 
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def enviar_comprovantes_devolucao(comprovantes):
--- services/circulacao/circulacaoapp/tasks/emprestimo.py
+++ services/circulacao/circulacaoapp/tasks/emprestimo.py
@@ -1,29 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-import os
-import requests
 from django.utils.timezone import localtime
 
 from circulacaoapp.models import Emprestimo
-
-USUARIO_SISTEMA_ID = os.getenv('USUARIO_SISTEMA_ID')
-CATALOGO_SERVICE_URL = os.getenv('CATALOGO_SERVICE_URL')
-
-def _marcar_exemplares_emprestados(codigos):
-    r = requests.put(
-        CATALOGO_SERVICE_URL + '/exemplares/emprestados', 
-        headers={ 'X-Usuario-Id': USUARIO_SISTEMA_ID },
-        json={ 'codigos': codigos }
-    )
-    r.raise_for_status()
-
-def _marcar_exemplares_devolvidos(codigos):
-    r = requests.put(
-        CATALOGO_SERVICE_URL + '/exemplares/devolvidos', 
-        headers={ 'X-Usuario-Id': USUARIO_SISTEMA_ID },
-        json={ 'codigos': codigos }
-    )
-    r.raise_for_status()
 
 def _emprestimo_avaliado(emprestimo_id):
     Emprestimo.objects.filter(_id=emprestimo_id).update(
