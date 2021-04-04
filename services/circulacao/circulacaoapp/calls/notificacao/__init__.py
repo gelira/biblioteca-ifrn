7101bbceb15b064f9f38bdc74639922b3fd9c39c
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-04 14:37:26 -0300

    pacote calls circulacao

---
+++ services/circulacao/circulacaoapp/calls/notificacao/__init__.py
@@ -0,0 +1,4 @@
+from .mensagem import (
+    task_comprovante_devolucao,
+    task_comprovante_emprestimo
+)
---
+++ services/circulacao/circulacaoapp/calls/notificacao/mensagem.py
@@ -0,0 +1,18 @@
+import os
+from circulacao.celery import app
+
+NOTIFICACAO_QUEUE = os.getenv('NOTIFICACAO_QUEUE')
+
+def task_comprovante_emprestimo(contexto_email, emails):
+    app.send_task(
+        'notificacaoapp.tasks.comprovante_emprestimo', 
+        [contexto_email, emails], 
+        queue=NOTIFICACAO_QUEUE
+    )
+
+def task_comprovante_devolucao(contexto_email, emails):
+    app.send_task(
+        'notificacaoapp.tasks.comprovante_devolucao', 
+        [comprovante, emails], 
+        queue=NOTIFICACAO_QUEUE
+    )
