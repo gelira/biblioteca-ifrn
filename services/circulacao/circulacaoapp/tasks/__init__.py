54e1a99d7edbcd5c4b2e9ae927f84e910eebeb59
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-11 09:15:27 -0300

    task enviar reservas disponíveis

--- services/circulacao/circulacaoapp/tasks/__init__.py
+++ services/circulacao/circulacaoapp/tasks/__init__.py
@@ -5,7 +5,8 @@ from circulacaoapp.tasks.emprestimo import (
     _emprestimo_avaliado
 )
 from circulacaoapp.tasks.reserva import (
-    _verificar_reservas
+    _verificar_reservas,
+    _enviar_reservas_disponiveis
 )
 from circulacaoapp.tasks.devolucao import (
     _enviar_comprovantes_devolucao
@@ -22,3 +23,7 @@ def verificar_reservas():
 @shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
 def emprestimo_avaliado(emprestimo_id):
     _emprestimo_avaliado(emprestimo_id)
+
+@shared_task(autoretry_for=(Exception,), max_retries=None, default_retry_delay=30)
+def enviar_reservas_disponiveis(comprovantes):
+    _enviar_reservas_disponiveis(comprovantes)
--- services/circulacao/circulacaoapp/tasks/reserva.py
+++ services/circulacao/circulacaoapp/tasks/reserva.py
@@ -1,9 +1,13 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
 from django.db import transaction
 from django.utils import timezone
 from circulacaoapp.models import Reserva, Data
 from circulacaoapp.utils import calcular_data_limite
+from circulacaoapp import calls
+
+PROJECT_NAME = os.getenv('PROJECT_NAME')
 
 def _verificar_reservas():
     data = timezone.localdate() - timezone.timedelta(days=1)
@@ -40,3 +44,36 @@ def _verificar_reservas():
             if proxima_reserva is not None:
                 proxima_reserva.disponibilidade_retirada = calcular_data_limite(1)
                 proxima_reserva.save()
+
+
+def _enviar_reservas_disponiveis(comprovantes):
+    usuarios = {}
+    livros = {}
+
+    for comprovante in comprovantes:
+        usuario_id = comprovante['usuario_id']
+        livro_id = comprovante['livro_id']
+
+        if usuario_id not in usuarios:
+            r = calls.autenticacao.api_consulta_usuario(usuario_id)
+            r.raise_for_status()
+            usuarios[usuario_id] = r.json()
+
+        if livro_id not in livros:
+            r = calls.catalogo.api_get_livro(livro_id, { 'min': '1' })
+            r.raise_for_status()
+            livros[livro_id] = r.json()
+
+        usuario = usuarios[usuario_id]
+        livro = livros[livro_id]
+
+        emails = [usuario['email_institucional']]
+        if usuario['email_pessoal']:
+            emails.append(usuario['email_pessoal'])
+
+        comprovante.update({
+            'nome_usuario': usuario['nome'],
+            'titulo_livro': livro['titulo']
+        })
+
+        calls.notificacao.task_reserva_disponivel(comprovante, emails)
