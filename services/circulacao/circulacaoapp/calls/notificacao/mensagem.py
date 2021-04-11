d09c59a5c7af5aef5e239215d80579924785b29e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-11 10:24:23 -0300

    call notificação reserva cancelada

--- services/circulacao/circulacaoapp/calls/notificacao/mensagem.py
+++ services/circulacao/circulacaoapp/calls/notificacao/mensagem.py
@@ -23,3 +23,10 @@ def task_reserva_disponivel(contexto_email, emails):
         [contexto_email, emails], 
         queue=NOTIFICACAO_QUEUE
     )
+
+def task_reserva_cancelada(contexto_email, emails):
+    app.send_task(
+        'notificacaoapp.tasks.reserva_cancelada', 
+        [contexto_email, emails], 
+        queue=NOTIFICACAO_QUEUE
+    )
