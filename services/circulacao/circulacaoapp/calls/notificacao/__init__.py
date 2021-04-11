71ec92ba23029982a3f3099650663874a3908f95
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-04-11 09:13:02 -0300

    call notificação reserva disponível

--- services/circulacao/circulacaoapp/calls/notificacao/__init__.py
+++ services/circulacao/circulacaoapp/calls/notificacao/__init__.py
@@ -1,4 +1,5 @@
 from .mensagem import (
     task_comprovante_devolucao,
-    task_comprovante_emprestimo
+    task_comprovante_emprestimo,
+    task_reserva_disponivel
 )
--- services/circulacao/circulacaoapp/calls/notificacao/mensagem.py
+++ services/circulacao/circulacaoapp/calls/notificacao/mensagem.py
@@ -16,3 +16,10 @@ def task_comprovante_devolucao(contexto_email, emails):
         [contexto_email, emails], 
         queue=NOTIFICACAO_QUEUE
     )
+
+def task_reserva_disponivel(contexto_email, emails):
+    app.send_task(
+        'notificacaoapp.tasks.reserva_disponivel', 
+        [contexto_email, emails], 
+        queue=NOTIFICACAO_QUEUE
+    )
