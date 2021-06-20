57907bb0a9bdf270fdcce4f3fbca2807808213c5
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-06-20 12:06:38 -0300

    removendo calls desnecessárias circulacao

--- services/circulacao/circulacaoapp/calls/autenticacao/__init__.py
+++ services/circulacao/circulacaoapp/calls/autenticacao/__init__.py
@@ -1,7 +1,3 @@
 from .usuario import (
-    api_autenticar_usuario,
-    api_consulta_usuario,
-    api_informacoes_usuario,
-    task_usuarios_abono,
-    task_usuarios_suspensos
+    api_consulta_usuario
 )
