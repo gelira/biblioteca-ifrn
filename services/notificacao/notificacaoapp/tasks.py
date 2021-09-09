f4a55736d5f20bba44f9b0ab1733bb735c725167
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-09 13:10:37 -0300

    removendo email das tasks de notificação

--- services/notificacao/notificacaoapp/tasks.py
+++ services/notificacao/notificacaoapp/tasks.py
@@ -11,20 +11,20 @@ class IgnoreResultTask(BaseTask):
     ignore_result = True
 
 @shared_task(name='notificacao.comprovante_emprestimo', base=IgnoreResultTask)
-def comprovante_emprestimo(contexto, emails):
-    MensagemService.comprovante_emprestimo(contexto, emails)
+def comprovante_emprestimo(contexto):
+    MensagemService.comprovante_emprestimo(contexto)
 
 @shared_task(name='notificacao.comprovante_devolucao', base=IgnoreResultTask)
-def comprovante_devolucao(contexto, emails):
-    MensagemService.comprovante_devolucao(contexto, emails)
+def comprovante_devolucao(contexto):
+    MensagemService.comprovante_devolucao(contexto)
 
 @shared_task(name='notificacao.reserva_disponivel', base=IgnoreResultTask)
-def reserva_disponivel(contexto, emails):
-    MensagemService.reserva_disponivel(contexto, emails)
+def reserva_disponivel(contexto):
+    MensagemService.reserva_disponivel(contexto)
 
 @shared_task(name='notificacao.reserva_cancelada', base=IgnoreResultTask)
-def reserva_cancelada(contexto, emails):
-    MensagemService.reserva_cancelada(contexto, emails)
+def reserva_cancelada(contexto):
+    MensagemService.reserva_cancelada(contexto)
 
 @shared_task(name='notificacao.salvar_contato', base=IgnoreResultTask)
 def salvar_contato(usuario_id, data):
