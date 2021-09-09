21592541c7c063c30c5bb977928c1e424bb13b1b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-09 13:15:04 -0300

    removendo email chamadas notificação

--- services/circulacao/circulacaoapp/services/notificacao.py
+++ services/circulacao/circulacaoapp/services/notificacao.py
@@ -6,37 +6,37 @@ NOTIFICACAO_QUEUE = os.getenv('NOTIFICACAO_QUEUE')
 
 class NotificacaoService:
     @classmethod
-    def comprovante_emprestimo(cls, contexto, emails):
+    def comprovante_emprestimo(cls, contexto):
         app.send_task(
             'notificacao.comprovante_emprestimo',
-            args=[contexto, emails],
+            args=[contexto],
             queue=NOTIFICACAO_QUEUE,
             ignore_result=True
         )
 
     @classmethod
-    def comprovante_devolucao(cls, contexto, emails):
+    def comprovante_devolucao(cls, contexto):
         app.send_task(
             'notificacao.comprovante_devolucao',
-            args=[contexto, emails],
+            args=[contexto],
             queue=NOTIFICACAO_QUEUE,
             ignore_result=True
         )
 
     @classmethod
-    def reserva_disponivel(cls, contexto, emails):
+    def reserva_disponivel(cls, contexto):
         app.send_task(
             'notificacao.reserva_disponivel',
-            args=[contexto, emails],
+            args=[contexto],
             queue=NOTIFICACAO_QUEUE,
             ignore_result=True
         )
 
     @classmethod
-    def reserva_cancelada(cls, contexto, emails):
+    def reserva_cancelada(cls, contexto):
         app.send_task(
             'notificacao.reserva_cancelada',
-            args=[contexto, emails],
+            args=[contexto],
             queue=NOTIFICACAO_QUEUE,
             ignore_result=True
         )
