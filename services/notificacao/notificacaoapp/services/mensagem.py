9522f3746845d1ca5d46141b971f5d439c46dd75
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-09 13:10:02 -0300

    carregando informações do contato no contexto

--- services/notificacao/notificacaoapp/services/mensagem.py
+++ services/notificacao/notificacaoapp/services/mensagem.py
@@ -27,16 +27,42 @@ class MensagemService:
         contexto['matricula_atendente'] = contato.matricula
         return contexto
 
+    @classmethod
+    def comprovante_emprestimo(cls, contexto):
+        usuario = cls.get_contato(contexto['usuario_id'])
+        atendente = cls.get_contato(contexto['atendente_id'])
+
+        contexto = cls.load_info_usuario(usuario, contexto)
+        contexto = cls.load_info_atendente(atendente, contexto)
+
+        emails = cls.get_emails_contato(usuario)
+
         EmailService.comprovante_emprestimo(contexto, emails)
 
     @classmethod
-    def comprovante_devolucao(cls, contexto, emails):
+    def comprovante_devolucao(cls, contexto):
+        usuario = cls.get_contato(contexto['usuario_id'])
+        atendente = cls.get_contato(contexto['atendente_id'])
+
+        contexto = cls.load_info_usuario(usuario, contexto)
+        contexto = cls.load_info_atendente(atendente, contexto)
+
+        emails = cls.get_emails_contato(usuario)
+
         EmailService.comprovante_devolucao(contexto, emails)
 
     @classmethod
-    def reserva_disponivel(cls, contexto, emails):
+    def reserva_disponivel(cls, contexto):
+        usuario = cls.get_contato(contexto['usuario_id'])
+        contexto = cls.load_info_usuario(usuario, contexto)
+        emails = cls.get_emails_contato(usuario)
+
         EmailService.reserva_disponivel(contexto, emails)
 
     @classmethod
-    def reserva_cancelada(cls, contexto, emails):
+    def reserva_cancelada(cls, contexto):
+        usuario = cls.get_contato(contexto['usuario_id'])
+        contexto = cls.load_info_usuario(usuario, contexto)
+        emails = cls.get_emails_contato(usuario)
+
         EmailService.reserva_cancelada(contexto, emails)
