ba00196c86dd921ca8f2ec0ec6d4aacb82dfc8f8
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-09 13:09:35 -0300

    buscando informações de contato para notificação

--- services/notificacao/notificacaoapp/services/mensagem.py
+++ services/notificacao/notificacaoapp/services/mensagem.py
@@ -1,8 +1,32 @@
+from ..models import Contato
+
 from .email import EmailService
 
 class MensagemService:
     @classmethod
-    def comprovante_emprestimo(cls, contexto, emails):
+    def get_contato(cls, usuario_id):
+        return Contato.objects.filter(usuario_id=usuario_id).first()
+
+    @classmethod
+    def get_emails_contato(cls, contato):
+        emails = [contato.email_institucional]
+        
+        if contato.email_pessoal:
+            emails.append(contato.email_pessoal)
+        
+        return emails
+
+    @classmethod
+    def load_info_usuario(cls, contato, contexto):
+        contexto['nome_usuario'] = contato.nome
+        return contexto
+
+    @classmethod
+    def load_info_atendente(cls, contato, contexto):
+        contexto['nome_atendente'] = contato.nome
+        contexto['matricula_atendente'] = contato.matricula
+        return contexto
+
         EmailService.comprovante_emprestimo(contexto, emails)
 
     @classmethod
