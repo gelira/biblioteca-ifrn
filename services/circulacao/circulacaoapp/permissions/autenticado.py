84828c52c37a50a6079926fe9ab3e77dabb9fc02
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-10 11:14:25 -0300

    correções de permissão de autenticação

--- services/circulacao/circulacaoapp/permissions/autenticado.py
+++ services/circulacao/circulacaoapp/permissions/autenticado.py
@@ -1,7 +1,15 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework.permissions import BasePermission
 
 class AutenticadoPermissao(BasePermission):
     message = 'Usuário não autenticado'
 
     def has_permission(self, request, view):
-        return request.user is not None
+        u = request.user
+        if u is None:
+            return False
+
+        if isinstance(u, AnonymousUser):
+            return False
+
+        return True
