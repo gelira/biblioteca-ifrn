5309992d39d3b8e8dba8d321925e554fd5f8e3ac
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-14 17:32:58 -0300

    disparando tasks de notificação

--- services/avaliacao/avaliacaoapp/views/avaliacao.py
+++ services/avaliacao/avaliacaoapp/views/avaliacao.py
@@ -1,3 +1,4 @@
+from django.utils.timezone import localtime
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.views import Response
@@ -8,6 +9,7 @@ from ..permissions import (
     ModerarPermissao
 )
 from ..serializers import AvaliacaoCreateSerializer
+from ..services import ModeracaoService
 
 class AvaliacaoViewSet(ModelViewSet):
     lookup_value_regex = '[a-f0-9]{8}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{4}\-[a-f0-9]{12}'
@@ -25,5 +27,21 @@ class AvaliacaoViewSet(ModelViewSet):
         
         avaliacao.censurada = not avaliacao.censurada
         avaliacao.save() 
+
+        agora = localtime()
+
+        contexto = {
+            'moderador_id': request.user['_id'],
+            'usuario_id': str(avaliacao.usuario_id),
+            'livro_id': str(avaliacao.livro_id),
+            'data': agora.strftime('%d/%m/%Y'),
+            'hora': agora.strftime('%H:%M:%S'),
+            'comentario': avaliacao.comentario
+        }
+
+        if avaliacao.censurada:
+            ModeracaoService.call_avaliacao_moderada(contexto)
+        else:
+            ModeracaoService.call_avaliacao_publicada(contexto)
 
         return Response(status=200)
