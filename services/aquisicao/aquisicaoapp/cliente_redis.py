2e5324a096bee7100e25f049b7340ab88c72d098
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-09-30 10:24:05 -0300

    cliente redis

---
+++ services/aquisicao/aquisicaoapp/cliente_redis.py
@@ -0,0 +1,28 @@
+import os
+import json
+from redis import Redis
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_DB = int(os.getenv('REDIS_DB'))
+
+class ClienteRedis:
+    def __init__(self):
+        self.con = Redis(host=REDIS_HOST, db=REDIS_DB)
+
+    def store(self, chave, valor, update=False):
+        if not isinstance(valor, str):
+            valor = json.dumps(valor)
+        
+        if update:
+            self.con.set(chave, valor, keepttl=True)
+        else:
+            self.con.set(chave, valor, ex=4*60*60)
+
+    def exist(self, chave):
+        return self.con.exists(chave) > 0
+
+    def get(self, chave):
+        valor = self.con.get(chave)
+        if valor is None:
+            return None
+        return json.loads(valor)
