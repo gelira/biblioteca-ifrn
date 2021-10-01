b594c5cf341e73706a0d10caba6ac4e08368c4dc
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-10-01 11:26:06 -0300

    registro de rotas

--- services/aquisicao/aquisicao/urls.py
+++ services/aquisicao/aquisicao/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from aquisicaoapp import views
+
+router = SimpleRouter(trailing_slash=False)
+router.register('sugestoes', views.SugestaoAquisicaoViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    #path('admin/', admin.site.urls),
+] + router.urls
