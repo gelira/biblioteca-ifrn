b99ecd830ee83c86aeeca7c3e0b78c4cc87c0731
Geraldo Eloi <geraldo.eloi11@gmail.com>
2021-10-03 14:35:20 -0300

    especializando método de verificação de permissão

--- services/aquisicao/aquisicaoapp/permissions/sugestao_aquisicao.py
+++ services/aquisicao/aquisicaoapp/permissions/sugestao_aquisicao.py
@@ -1,6 +1,9 @@
 from rest_framework.permissions import BasePermission
 
 class ModificarSugestaoAquisicaoPermissao(BasePermission):
+    def user_has_permission(self, request):
+        return 'lista_sugestoes.criar' in request.user['lista_permissoes']
+
     def has_object_permission(self, request, view, obj):
         if view.action not in ['update', 'partial_update']:
             return True
@@ -11,7 +14,7 @@ class ModificarSugestaoAquisicaoPermissao(BasePermission):
             self.message = 'Sugestão já separada em lista'
             return not in_lista
 
-        if 'lista_sugestoes.criar' in request.user['lista_permissoes']:
+        if self.user_has_permission(request):
             self.message = 'Sugestão ainda não pertence a uma lista'
             return in_lista
 
