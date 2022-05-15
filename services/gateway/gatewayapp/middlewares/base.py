cab2a3a949f826c2ed315ecbda22db20ab14b7b9
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-15 14:29:33 -0300

    tratando APIExceptions

--- services/gateway/gatewayapp/middlewares/base.py
+++ services/gateway/gatewayapp/middlewares/base.py
@@ -1,3 +1,6 @@
+from django.http.response import JsonResponse
+from rest_framework.exceptions import APIException
+
 ALLOW_URLS = [
     '/autenticacao/token',
     '/autenticacao/verificar',
@@ -11,7 +14,18 @@ class BaseMiddleware:
     def __call__(self, request):
         if self.allow_url(request):
             return self.get_response(request)
-        return self.process_request(request)
+
+        try:
+            return self.process_request(request)
+        
+        except APIException as e:
+            return JsonResponse(
+                data={
+                    'message': e.default_detail,
+                    'code': e.default_code,
+                }, 
+                status=e.default_code
+            )
 
     def allow_url(self, request):
         for url in ALLOW_URLS:
