6c256bb4e3dfa6c18377759b7f5d2b0344db6951
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-15 14:30:59 -0300

    código 503 retorno no proxy

--- services/gateway/gatewayapp/views/base.py
+++ services/gateway/gatewayapp/views/base.py
@@ -6,13 +6,20 @@ class BaseProxyView(ProxyView):
     def dispatch(self, request, path):
         try:
             return super().dispatch(request, path)
+
         except MaxRetryError:
-            return JsonResponse({ 'detail': 'Serviço indisponível' }, status=408)
+            return JsonResponse({ 
+                'message': 'Serviço indisponível',
+                'code': 'service_unavailable' 
+            }, status=503)
 
     def get_request_headers(self):
         headers = super().get_request_headers()
         headers.pop('Authorization', None)
+
         usuario_id = self.request.META.get('_id')
+
         if usuario_id is not None:
             headers['X-Usuario-Id'] = usuario_id
+
         return headers
