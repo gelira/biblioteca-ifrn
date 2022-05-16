c55c6ddf49d83674bafc6319302969eb95ae335e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-16 00:19:50 -0300

    modificando autenticacao service em circulacao

--- services/circulacao/circulacaoapp/services/autenticacao.py
+++ services/circulacao/circulacaoapp/services/autenticacao.py
@@ -1,6 +1,26 @@
 import os
-from celery import group
-from circulacao.celery import app
+import requests
+
+from .. import exceptions
+
+AUTENTICACAO_SERVICE_URL = os.getenv('AUTENTICACAO_SERVICE_URL')
+
+AUTENTICACAO_TIMEOUT = int(os.getenv('AUTENTICACAO_TIMEOUT'))
+
+AUTENTICACAO_TOKEN = (
+    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_TOKEN'))
+
+AUTENTICACAO_INFORMACOES_USUARIO = (
+    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_INFORMACOES_USUARIO'))
+
+AUTENTICACAO_CONSULTA_USUARIO = (
+    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_CONSULTA_USUARIO'))
+
+AUTENTICACAO_SUSPENSOES = (
+    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_SUSPENSOES'))
+
+AUTENTICACAO_ABONO_SUSPENSOES = (
+    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_ABONO_SUSPENSOES'))
 
 AUTENTICACAO_QUEUE = os.getenv('AUTENTICACAO_QUEUE')
 
@@ -8,56 +28,78 @@ class AutenticacaoService:
     @classmethod
     def autenticar_usuario(cls, matricula, senha):
         data_token = cls.login_suap(matricula, senha)
-        data_verify = cls.verificar_token(data_token['token'])
-        return cls.informacoes_usuario(data_verify['user_id'])
+        return cls.informacoes_usuario(token=data_token['token'])
 
     @classmethod
     def login_suap(cls, username, password):
-        task = app.send_task(
-            'autenticacao.login_suap', 
-            args=[username, password], 
-            queue=AUTENTICACAO_QUEUE
-        )
-        return task.get()
+        return cls.dispatch({
+            'method': 'POST',
+            'url': AUTENTICACAO_TOKEN,
+            'json': {
+                'username': username,
+                'password': password
+            }
+        })
 
     @classmethod
-    def verificar_token(cls, token):
-        task = app.send_task(
-            'autenticacao.verificar_token', 
-            args=[token], 
-            queue=AUTENTICACAO_QUEUE
-        )
-        return task.get()
+    def informacoes_usuario(cls, usuario_id=None, token=None):
+        if not usuario_id and not token:
+            raise Exception('Uma das opções deve ser definida: usuario_id ou token')
+        
+        options = { 
+            'method': 'GET' 
+        }
 
-    @classmethod
-    def informacoes_usuario(cls, usuario_id):
-        task = app.send_task(
-            'autenticacao.informacoes_usuario', 
-            args=[usuario_id], 
-            queue=AUTENTICACAO_QUEUE
-        )
-        return task.get(disable_sync_subtasks=False)
+        if usuario_id:
+            options.update({
+                'url': AUTENTICACAO_CONSULTA_USUARIO,
+                'params': {
+                    'id': usuario_id
+                }
+            })
+
+        else:
+            options.update({
+                'url': AUTENTICACAO_INFORMACOES_USUARIO,
+                'headers': {
+                    'Authorization': f'JWT {token}'
+                }
+            })
+
+        return cls.dispatch(options)
 
     @classmethod
     def suspensoes(cls, suspensoes):
-        tasks = [
-            app.signature(
-                'autenticacao.suspensao', 
-                args=[u, suspensoes[u]], 
-                ignore_resut=True, 
-                queue=AUTENTICACAO_QUEUE
-            ) for u in suspensoes
-        ]
-        group(tasks)()
+        return cls.dispatch({
+            'method': 'POST',
+            'url': AUTENTICACAO_SUSPENSOES,
+            'json': suspensoes
+        })
     
     @classmethod
-    def abono_suspensoes(cls, usuarios):
-        tasks = [
-            app.signature(
-                'autenticacao.abono_suspensao', 
-                args=[u, usuarios[u]], 
-                ignore_resut=True, 
-                queue=AUTENTICACAO_QUEUE
-            ) for u in usuarios
-        ]
-        group(tasks)()
+    def abono_suspensoes(cls, suspensoes):
+        return cls.dispatch({
+            'method': 'POST',
+            'url': AUTENTICACAO_ABONO_SUSPENSOES,
+            'json': suspensoes
+        })
+
+    @classmethod
+    def dispatch(self, options):
+        method = options.pop('method')
+        url = options.pop('url')
+        options['timeout'] = AUTENTICACAO_TIMEOUT
+        
+        try:
+            response = requests.request(method, url, **options)
+            response.raise_for_status()
+            return response.json()
+
+        except requests.exceptions.HTTPError:
+            raise exceptions.ServiceUnauthorized
+        
+        except requests.exceptions.ConnectTimeout:
+            raise exceptions.ServiceTimeOut
+        
+        except requests.exceptions.ConnectionError:
+            raise exceptions.ServiceUnavailable
