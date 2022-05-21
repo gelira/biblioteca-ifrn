d9755179b8c82782c35658cb58d3355d98e8b0d8
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-21 15:27:53 -0300

    urls autenticacao service gateway

--- services/gateway/gatewayapp/services/autenticacao.py
+++ services/gateway/gatewayapp/services/autenticacao.py
@@ -4,21 +4,17 @@ import requests
 from .. import exceptions
 
 AUTENTICACAO_SERVICE_URL = os.getenv('AUTENTICACAO_SERVICE_URL')
-
 AUTENTICACAO_TIMEOUT = int(os.getenv('AUTENTICACAO_TIMEOUT'))
 
-AUTENTICACAO_VERIFICAR_TOKEN = (
-    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_VERIFICAR_TOKEN'))
-
-AUTENTICACAO_INFORMACOES_USUARIO = (
-    AUTENTICACAO_SERVICE_URL + os.getenv('AUTENTICACAO_INFORMACOES_USUARIO'))
-
 class AutenticacaoService:
+    url_verificar_token = AUTENTICACAO_SERVICE_URL + '/verificar'
+    url_informacoes_usuario = AUTENTICACAO_SERVICE_URL + '/informacoes'
+
     @classmethod
     def verificar_token(cls, token):
         return cls.dispatch({
             'method': 'GET',
-            'url': AUTENTICACAO_VERIFICAR_TOKEN,
+            'url': cls.url_verificar_token,
             'headers': {
                 'Authorization': f'JWT {token}'
             }
@@ -28,7 +24,7 @@ class AutenticacaoService:
     def informacoes_usuario(cls, token):
         return cls.dispatch({
             'method': 'GET',
-            'url': AUTENTICACAO_INFORMACOES_USUARIO,
+            'url': cls.url_informacoes_usuario,
             'headers': {
                 'Authorization': f'JWT {token}'
             }
