64d934e4312187270362181b3d12f2a9e9b81b77
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-21 11:40:56 -0300

    usando chamadas síncronas catalogo service

--- services/circulacao/circulacaoapp/exceptions.py
+++ services/circulacao/circulacaoapp/exceptions.py
@@ -6,6 +6,11 @@ class ServiceUnauthorized(APIException):
     default_detail = 'Acesso não autorizado'
     default_code = 'unauthorized'
 
+class ServiceBadRequest(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Ocorreu algum problema ao acessar esse recurso'
+    default_code = 'bad_request'
+
 class ServiceTimeOut(APIException):
     status_code = status.HTTP_504_GATEWAY_TIMEOUT
     default_detail = 'Serviço demorou muito a responder'
--- services/circulacao/circulacaoapp/services/catalogo.py
+++ services/circulacao/circulacaoapp/services/catalogo.py
@@ -1,42 +1,70 @@
 import os
-from circulacao.celery import app
+import requests
+
+from .. import exceptions
 
 CATALOGO_QUEUE = os.getenv('CATALOGO_QUEUE')
 
+CATALOGO_SERVICE_URL = os.getenv('CATALOGO_SERVICE_URL')
+CATALOGO_TIMEOUT = int(os.getenv('CATALOGO_TIMEOUT'))
+
 class CatalogoService:
+    url_consulta_exemplar = CATALOGO_SERVICE_URL + '/exemplares/consulta/'
+    url_exemplares_emprestados = CATALOGO_SERVICE_URL + '/exemplares/emprestados'
+    url_exemplares_devolvidos = CATALOGO_SERVICE_URL + '/exemplares/devolvidos'
+    url_buscar_livro = CATALOGO_SERVICE_URL + '/livros/'
+
     @classmethod
     def consulta_codigo_exemplar(cls, codigo):
-        task = app.send_task(
-            'catalogo.consulta_codigo_exemplar', 
-            args=[codigo], 
-            queue=CATALOGO_QUEUE
-        )
-        return task.get()
+        return cls.dispatch({
+            'url': cls.url_consulta_exemplar + codigo,
+            'method': 'GET'
+        })
 
     @classmethod
-    def exemplares_emprestados(cls, codigo):
-        app.send_task(
-            'catalogo.exemplares_emprestados', 
-            args=[codigo],
-            ignore_task=True, 
-            queue=CATALOGO_QUEUE
-        )
+    def exemplares_emprestados(cls, codigos):
+        return cls.dispatch({
+            'url': cls.url_exemplares_emprestados,
+            'method': 'PATCH',
+            'json': {
+                'codigos': codigos
+            }
+        })
 
     @classmethod
-    def exemplares_devolvidos(cls, codigo):
-        app.send_task(
-            'catalogo.exemplares_devolvidos', 
-            args=[codigo], 
-            ignore_task=True,
-            queue=CATALOGO_QUEUE
-        )
+    def exemplares_devolvidos(cls, codigos):
+        return cls.dispatch({
+            'url': cls.url_exemplares_devolvidos,
+            'method': 'PATCH',
+            'json': {
+                'codigos': codigos
+            }
+        })
 
     @classmethod
-    def busca_livro(cls, livro_id, **kwargs):
-        task = app.send_task(
-            'catalogo.busca_livro', 
-            args=[livro_id],
-            kwargs=kwargs, 
-            queue=CATALOGO_QUEUE
-        )
-        return task.get(disable_sync_subtasks=False)
+    def busca_livro(cls, livro_id, **params):
+        return cls.dispatch({
+            'url': cls.url_buscar_livro + livro_id,
+            'method': 'GET',
+            'params': params
+        })
+
+    @classmethod
+    def dispatch(cls, options):
+        method = options.pop('method')
+        url = options.pop('url')
+        options['timeout'] = CATALOGO_TIMEOUT
+        
+        try:
+            response = requests.request(method, url, **options)
+            response.raise_for_status()
+            return response.json()
+
+        except requests.exceptions.HTTPError:
+            raise exceptions.ServiceBadRequest
+        
+        except requests.exceptions.ConnectTimeout:
+            raise exceptions.ServiceTimeOut
+        
+        except requests.exceptions.ConnectionError:
+            raise exceptions.ServiceUnavailable
