45834063f72767e863dc73377c79e2b8e21c5d15
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-21 13:07:24 -0300

    catalogo service em avaliacao síncrono

---
+++ services/avaliacao/avaliacaoapp/exceptions.py
@@ -0,0 +1,17 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+class ServiceBadRequest(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = 'Ocorreu algum problema ao acessar esse recurso'
+    default_code = 'bad_request'
+
+class ServiceTimeOut(APIException):
+    status_code = status.HTTP_504_GATEWAY_TIMEOUT
+    default_detail = 'Serviço demorou muito a responder'
+    default_code = 'service_timeout'
+
+class ServiceUnavailable(APIException):
+    status_code = status.HTTP_503_SERVICE_UNAVAILABLE
+    default_detail = 'Serviço indisponível'
+    default_code = 'service_unavailable'
