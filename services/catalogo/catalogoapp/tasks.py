bd149beeb702681886025351f4493a9cb722e025
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-05-21 15:34:25 -0300

    removendo tasks

--- services/catalogo/catalogoapp/tasks.py
+++ services/catalogo/catalogoapp/tasks.py
@@ -1,9 +1,6 @@
 from celery import shared_task, Task
 
-from .services import (
-    ExemplarService,
-    LivroService
-)
+from .services import LivroService
 
 class BaseTask(Task):
     autoretry_for = [Exception]
@@ -16,26 +13,7 @@ class IgnoreResultTask(BaseTask):
 class MaxRetriesTask(IgnoreResultTask):
     max_retries = 10
 
-@shared_task(name='catalogo.exemplares_emprestados', base=IgnoreResultTask)
-def exemplares_emprestados(codigos):
-    ExemplarService.exemplares_emprestados(codigos)
-
-@shared_task(name='catalogo.exemplares_devolvidos', base=IgnoreResultTask)
-def exemplares_devolvidos(codigos):
-    ExemplarService.exemplares_devolvidos(codigos)
-
-@shared_task(name='catalogo.consulta_codigo_exemplar', base=BaseTask)
-def consulta_codigo_exemplar(codigo):
-    return ExemplarService.consulta_codigo_exemplar(codigo)
-
-@shared_task(name='catalogo.busca_livro', base=BaseTask)
-def busca_livro(livro_id, **kwargs):
-    return LivroService.busca_livro(livro_id, **kwargs)
-
 @shared_task(name='catalogo.upload_foto_capa', base=MaxRetriesTask)
 def upload_foto_capa(livro_id, livro_pk, foto_base64):
     LivroService.upload_foto_capa(livro_id, livro_pk, foto_base64)
 
-@shared_task(name='catalogo.atualizar_nota', base=IgnoreResultTask)
-def atualizar_nota(livro_id, nota):
-    LivroService.atualizar_nota(livro_id, nota)
