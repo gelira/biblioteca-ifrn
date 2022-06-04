850ed200440f59b718bd056f60aaa04b3e8a704e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-04 11:20:33 -0300

    signal para desabilitar tarefa após envio

--- services/circulacao/circulacao/celery.py
+++ services/circulacao/circulacao/celery.py
@@ -2,12 +2,31 @@ from __future__ import absolute_import, unicode_literals
 
 import os
 import dotenv
-from celery import Celery
+import django
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'circulacao.settings')
+
+django.setup()
+
+from celery import Celery
+from celery.signals import after_task_publish
+from django_celery_beat.models import PeriodicTask
 
 app = Celery('circulacao')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+@after_task_publish.connect
+def signal(headers=None, **kwargs):
+    if not headers:
+        return
+
+    try:
+        periodic_task_name = headers.get('periodic_task_name')
+    except:
+        periodic_task_name = None
+
+    if periodic_task_name:
+        PeriodicTask.objects.filter(name=periodic_task_name).update(enabled=False)
