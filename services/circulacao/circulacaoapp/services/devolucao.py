61f8552b1fc3a410b0626b0b35a18f6b653724ef
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-11 15:42:07 -0300

    get emprestimos para devolução

--- services/circulacao/circulacaoapp/services/devolucao.py
+++ services/circulacao/circulacaoapp/services/devolucao.py
@@ -1,7 +1,10 @@
 import os
 from celery import group
+from rest_framework.exceptions import APIException
 
 from circulacao.celery import app
+
+from ..models import Emprestimo
 
 from .catalogo import CatalogoService
 from .notificacao import NotificacaoService
@@ -9,6 +12,20 @@ from .notificacao import NotificacaoService
 CIRCULACAO_QUEUE = os.getenv('CIRCULACAO_QUEUE')
 
 class DevolucaoService:
+    @classmethod
+    def get_emprestimos_para_devolucao(cls, emprestimos_id):
+        qs = Emprestimo.objects.filter(
+            _id__in=emprestimos_id, 
+            data_devolucao=None
+        )
+
+        lista_emprestimos = list(qs)
+
+        if len(lista_emprestimos) == 0:
+            raise APIException('Nenhum empréstimo foi encontrado')
+
+        return lista_emprestimos
+
     @classmethod
     def enviar_comprovante_devolucao(cls, contexto):
         livro_id = contexto['livro_id']
