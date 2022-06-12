b1831e4b8afd3e890817168d016a2a67ed2d0686
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-12 10:28:58 -0300

    save devoluções em service

--- services/circulacao/circulacaoapp/serializers/emprestimo.py
+++ services/circulacao/circulacaoapp/serializers/emprestimo.py
@@ -5,16 +5,13 @@ from rest_framework.exceptions import APIException
 
 from ..utils import calcular_data_limite
 from ..models import (
-    Emprestimo, 
-    Suspensao,
+    Emprestimo,
     Renovacao, 
     Reserva
 )
 from ..services import (
     AutenticacaoService,
-    CatalogoService,
     EmprestimoService,
-    ReservaService,
     DevolucaoService
 )
 
@@ -115,62 +112,9 @@ class DevolucaoEmprestimosSerializer(serializers.Serializer):
 
     def create(self, data):
         emprestimos = data['emprestimos']
-        agora = timezone.localtime()
-        hoje = agora.date()
-        data = agora.strftime('%d/%m/%Y')
-        hora = agora.strftime('%H:%M:%S')
-        
-        suspensoes = {}
-        codigos = []
-        livros = []
-
-        comprovantes = []
         atendente_id = self.context['request'].user['_id']
 
-        with transaction.atomic():
-            for emprestimo in emprestimos:
-                diff = hoje - emprestimo.data_limite
-                if diff.days > 0:
-                    Suspensao.objects.create(
-                        emprestimo=emprestimo,
-                        usuario_id=emprestimo.usuario_id,
-                        total_dias=diff.days
-                    )
-
-                    u_id = str(emprestimo.usuario_id)
-                    if u_id not in suspensoes:
-                        suspensoes[u_id] = 0
-                    suspensoes[u_id] += diff.days
-
-                codigos.append(emprestimo.exemplar_codigo)
-                emprestimo.data_devolucao = hoje
-                emprestimo.save()
-
-                livro_id = str(emprestimo.livro_id)
-                if livro_id not in livros:
-                    livros.append(livro_id)
-
-                comprovantes.append({
-                    'usuario_id': str(emprestimo.usuario_id),
-                    'atendente_id': atendente_id,
-                    'livro_id': livro_id,
-                    'atraso': diff.days,
-                    'data': data,
-                    'hora': hora,
-                    'exemplar_codigo': emprestimo.exemplar_codigo,
-                    'referencia': emprestimo.exemplar_referencia,
-                })
-
-            if suspensoes:
-                AutenticacaoService.suspensoes(list(map(
-                    lambda x: ({ 'usuario_id': x, 'dias': suspensoes[x] }), suspensoes)))
-
-            CatalogoService.exemplares_devolvidos(codigos)
-            
-        DevolucaoService.call_enviar_comprovantes_devolucao(comprovantes)
-        ReservaService.call_proximas_reservas(livros)
-
-        return {}
+        return DevolucaoService.save_devolucoes(emprestimos, atendente_id)
 
 class RenovacaoEmprestimosSerializer(serializers.Serializer):
     emprestimos = serializers.ListField(
--- services/circulacao/circulacaoapp/services/devolucao.py
+++ services/circulacao/circulacaoapp/services/devolucao.py
@@ -1,13 +1,17 @@
 import os
 from celery import group
+from django.db import transaction
+from django.utils import timezone
 from rest_framework.exceptions import APIException
 
 from circulacao.celery import app
 
-from ..models import Emprestimo
+from ..models import Emprestimo, Suspensao
 
+from .autenticacao import AutenticacaoService
 from .catalogo import CatalogoService
 from .notificacao import NotificacaoService
+from .reserva import ReservaService
 
 CIRCULACAO_QUEUE = os.getenv('CIRCULACAO_QUEUE')
 
@@ -25,6 +29,63 @@ class DevolucaoService:
             raise APIException('Nenhum empréstimo foi encontrado')
 
         return lista_emprestimos
+
+    @classmethod
+    def save_devolucoes(cls, emprestimos, atendente_id):
+        agora = timezone.localtime()
+        hoje = agora.date()
+        data = agora.strftime('%d/%m/%Y')
+        hora = agora.strftime('%H:%M:%S')
+        
+        suspensoes = {}
+        codigos = []
+        livros = []
+        comprovantes = []
+
+        with transaction.atomic():
+            for emprestimo in emprestimos:
+                diff = hoje - emprestimo.data_limite
+                if diff.days > 0:
+                    Suspensao.objects.create(
+                        emprestimo=emprestimo,
+                        usuario_id=emprestimo.usuario_id,
+                        total_dias=diff.days
+                    )
+
+                    u_id = str(emprestimo.usuario_id)
+                    if u_id not in suspensoes:
+                        suspensoes[u_id] = 0
+                    suspensoes[u_id] += diff.days
+
+                codigos.append(emprestimo.exemplar_codigo)
+                emprestimo.data_devolucao = hoje
+                emprestimo.save()
+
+                livro_id = str(emprestimo.livro_id)
+                if livro_id not in livros:
+                    livros.append(livro_id)
+
+                comprovantes.append({
+                    'usuario_id': str(emprestimo.usuario_id),
+                    'atendente_id': atendente_id,
+                    'livro_id': livro_id,
+                    'atraso': diff.days,
+                    'data': data,
+                    'hora': hora,
+                    'exemplar_codigo': emprestimo.exemplar_codigo,
+                    'referencia': emprestimo.exemplar_referencia,
+                })
+
+            if suspensoes:
+                AutenticacaoService.suspensoes(list(map(
+                    lambda x: ({ 'usuario_id': x, 'dias': suspensoes[x] }), suspensoes)))
+
+            CatalogoService.exemplares_devolvidos(codigos)
+            
+            cls.call_enviar_comprovantes_devolucao(comprovantes)
+            ReservaService.call_proximas_reservas(livros)
+
+        return {}
 
     @classmethod
     def enviar_comprovante_devolucao(cls, contexto):
