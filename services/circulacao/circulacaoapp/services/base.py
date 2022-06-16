09bb5b2164b4f952d05acd53a2d381b5a04478b6
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-16 16:13:19 -0300

    create batch periodic tasks

--- services/circulacao/circulacaoapp/services/base.py
+++ services/circulacao/circulacaoapp/services/base.py
@@ -6,6 +6,34 @@ from django_celery_beat.models import PeriodicTask, ClockedSchedule
 
 from circulacao.celery import app
 
+def datetime_name(name):
+    dt = timezone.localtime()
+    dt_str = dt.strftime('%Y%m%d.%H%M%S.%f')
+
+    return f'{dt_str}-{name}'
+
+def handle_datetime(dt=None, delay_seconds=60):
+    if not dt:
+        dt = timezone.localtime() + timezone.timedelta(seconds=delay_seconds)
+
+    if not dt.tzinfo:
+        dt = timezone.make_aware(dt)
+
+    return dt
+
+def handle_kwargs(kw):
+    args = kw.get('args', [])
+    kwargs = kw.get('kwargs', {})
+    headers = kw.get('headers', {})
+
+    kw.update({
+        'args': json.dumps(args),
+        'kwargs': json.dumps(kwargs),
+        'headers': json.dumps(headers),
+    })
+
+    return kw
+
 @circuit(failure_threshold = 1)
 def send_task(task_name, **kw):
     app.send_task(task_name, **kw)
@@ -25,28 +53,24 @@ def save_clocked_task(dt=None, delay_seconds=60, **kw):
     queue   -> nome da fila
     one_off -> boolean
     '''
-    if not dt:
-        dt = timezone.localtime() + timezone.timedelta(seconds=delay_seconds)
-
-    if not dt.tzinfo:
-        dt = timezone.make_aware(dt)
-
-    args = kw.get('args', [])
-    kwargs = kw.get('kwargs', {})
-    headers = kw.get('headers', {})
-
-    kw.update({
-        'args': json.dumps(args),
-        'kwargs': json.dumps(kwargs),
-        'headers': json.dumps(headers),
-    })
+    dt = handle_datetime(dt, delay_seconds)
+    kw = handle_kwargs(kw)
 
     clock = ClockedSchedule.objects.create(clocked_time=dt)
 
     PeriodicTask.objects.create(clocked=clock, **kw)
 
-def datetime_name(name):
-    dt = timezone.localtime()
-    dt_str = dt.strftime('%Y%m%d.%H%M%S.%f')
+def save_batch_clocked_tasks(dt=None, delay_seconds=60, contexts=[]):
+    if not contexts:
+        return
 
-    return f'{dt_str}-{name}'
+    dt = handle_datetime(dt, delay_seconds)
+
+    clock = ClockedSchedule.objects.create(clocked_time=dt)
+
+    PeriodicTask.objects.bulk_create([
+        PeriodicTask(
+            clocked=clock, 
+            **handle_kwargs(context)
+        ) for context in contexts
+    ])
