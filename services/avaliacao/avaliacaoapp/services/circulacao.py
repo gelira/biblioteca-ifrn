31606b940ec02bc24772c0c070ccf83d721ba644
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-18 11:40:50 -0300

    call emprestimo_avaliado

--- services/avaliacao/avaliacaoapp/services/circulacao.py
+++ services/avaliacao/avaliacaoapp/services/circulacao.py
@@ -3,13 +3,19 @@ import requests
 
 from .. import exceptions
 
+from .base import datetime_name, save_clocked_task
+
 AVALIACAO_USUARIO_ID = os.getenv('AVALIACAO_USUARIO_ID')
 
 CIRCULACAO_SERVICE_URL = os.getenv('CIRCULACAO_SERVICE_URL')
 CIRCULACAO_TIMEOUT = int(os.getenv('CIRCULACAO_TIMEOUT'))
 
+AVALIACAO_QUEUE = os.getenv('AVALIACAO_QUEUE')
+
 class CirculacaoService:
     url_emprestimos = CIRCULACAO_SERVICE_URL + '/emprestimos'
+
+    task_emprestimo_avaliado = 'avaliacao.emprestimo_avaliado'
 
     @classmethod
     def get_emprestimo(cls, emprestimo_id, usuario_id):
@@ -36,6 +42,20 @@ class CirculacaoService:
                 'X-Usuario-Id': AVALIACAO_USUARIO_ID,
             },
         })
+
+    @classmethod
+    def call_emprestimo_avaliado(cls, emprestimo_id):
+        try:
+            cls.emprestimo_avaliado(emprestimo_id)
+        
+        except:
+            name = datetime_name(cls.task_emprestimo_avaliado)
+            save_clocked_task(
+                name=name,
+                task=cls.task_emprestimo_avaliado,
+                args=[emprestimo_id],
+                queue=AVALIACAO_QUEUE
+            )
 
     @classmethod
     def dispatch(cls, options):
