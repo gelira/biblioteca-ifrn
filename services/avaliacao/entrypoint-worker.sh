f8dab3a2c8a7d72970b3e676101cacca6badd197
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-06-20 00:02:55 -0300

    removendo comando desnecessário

--- services/avaliacao/entrypoint-worker.sh
+++ services/avaliacao/entrypoint-worker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py one_off_tasks
 
 celery -A avaliacao worker -l info -n avaliacaoworker -Q ${AVALIACAO_QUEUE}
--- services/catalogo/entrypoint-worker.sh
+++ services/catalogo/entrypoint-worker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 python manage.py migrate
-python manage.py one_off_tasks
 
 celery -A catalogo worker -l info -n catalogoworker -Q ${CATALOGO_QUEUE}
