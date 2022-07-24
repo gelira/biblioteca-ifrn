63b098d914b589c2608f25e3bd41d79328c6a0cb
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-07-24 16:24:06 -0300

    Basic layout

---
+++ services/frontend/src/views/Main.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+function Main() {
+  return (
+    <>
+      <AppBar elevation="0">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            Scroll to elevate App bar
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Container>
+        <Box sx={{ my: 2 }}>
+          {[...new Array(12)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
+        </Box>
+      </Container>
+    </>
+  );
+}
+
+export default Main;
