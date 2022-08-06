3f4c6ec6db53960e0758f5e59d4d5462002e84fa
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-08-06 11:59:11 -0300

    Anonymous user

--- services/autenticacao/autenticacaoapp/permissions/autenticado.py
+++ services/autenticacao/autenticacaoapp/permissions/autenticado.py
@@ -1,10 +1,12 @@
+from django.contrib.auth.models import AnonymousUser
+
 from .base import BasePermission
 
 class AutenticadoPermissao(BasePermission):
     message = 'Usuário não autenticado'
 
     def has_permission(self, request, view):
-        if request.user is None:
+        if request.user is None or isinstance(request.user, AnonymousUser):
             return False
         _id = request.user['_id'] if isinstance(request.user, dict) else str(request.user.usuario._id)
         return self.cliente_redis.exist(_id)
