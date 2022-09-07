1edf93c6eb2a3caf1b6ac6449cb827fa7dda843e
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-09-07 15:10:18 -0300

    remove unused permission classes

--- services/autenticacao/autenticacaoapp/permissions/suspensao.py
+++
@@ -1,9 +0,0 @@
-from .base import BasePermission
-
-class AbonarSuspensaoPermissao(BasePermission):
-    message = 'Você não tem permissão para realizar essa ação'
-
-    def has_permission(self, request, view):
-        _id = request.user['_id'] if isinstance(request.user, dict) else str(request.user.usuario._id)
-        usuario = self.cliente_redis.get(_id)
-        return 'suspensao.abonar' in usuario['lista_permissoes']
